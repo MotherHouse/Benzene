30443c2ac05d76dfc6d0ca0a49a5db7d0750a380
Bob Zheng <leavenotrace@foxmail.com>
2017-06-19 16:42:40 +0800

    update dbconfig

--- thinkphpNews/Application/Common/Conf/db.php
+++ thinkphpNews/Application/Common/Conf/db.php
@@ -11,7 +11,7 @@ return array(
     'DB_USER' => 'root',
     'DB_PWD' => '',
     'DB_PORT' => 3306,
-    'DB_NAME' => 'rehellinen',
+    'DB_NAME' => 'thinkphpnews',
     'DB_CHARSET' => 'utf8',
     'DB_PREFIX' => 'cms_',
 );
